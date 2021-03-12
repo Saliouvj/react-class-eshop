83af77caa23da4c0a6d428b25cc7774a0adab851
saliouvj <m.saliou90@gmail.com>
2021-03-12 10:16:42 +0100

    eshopReact

---
+++ src/header/Header.js
@@ -0,0 +1,34 @@
+// Import du CSS et de REACT
+import './header.css';
+import React, { Component } from 'react';
+// Import de votre composants/modules
+
+// Import de vos Images
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+class Header extends Component {
+  // !! FONCTION A ECRIRE AVANT RENDER() !! //
+
+
+
+  // !! FONCTION A ECRIRE AVANT RENDER() !! //
+
+    render() {
+        // !! VARIABLE A ECRIRE AVANT RETURN !! //
+
+
+
+        // !! VARIABLE A ECRIRE AVANT RETURN !! //
+
+        return (
+            <div>
+            {/* DEBUT DE VOTRE JSX */}
+
+            {/* FIN DE VOTRE JSX */}
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/main/Section.js
@@ -0,0 +1,34 @@
+// Import du CSS et de REACT
+import './section.css';
+import React, { Component } from 'react';
+// Import de votre composants/modules
+
+// Import de vos Images
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+class Section extends Component {
+  // !! FONCTION A ECRIRE AVANT RENDER() !! //
+
+
+
+  // !! FONCTION A ECRIRE AVANT RENDER() !! //
+
+    render() {
+        // !! VARIABLE A ECRIRE AVANT RETURN !! //
+
+
+
+        // !! VARIABLE A ECRIRE AVANT RETURN !! //
+
+        return (
+            <div>
+            {/* DEBUT DE VOTRE JSX */}
+
+            {/* FIN DE VOTRE JSX */}
+            </div>
+        );
+    }
+}
+
+export default Section;
